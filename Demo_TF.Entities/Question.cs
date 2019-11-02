0aabcba0fbdcbf0a9085e40a611f6fd1f8cea142
Gonzarod <gonzalo986796836@outlook.com>
2019-11-01 19:35:47 -0500

    Entidades_1
    Implementacion entidades Question,Option, Survey, Survey_Option

---
+++ Demo_TF.Entities/Question.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo_TF.Entities
+{
+    public class Question
+    {
+        public int Question_id { get; set; }
+        public String Question_content { get; set; }
+        public virtual ICollection<Option> Options { get; set; }
+    }
+}
---
+++ Demo_TF.Entities/Survey_options.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo_TF.Entities
+{
+    public class Survey_options
+    {
+        public int Survey_option_id { get; set; }
+        public int option_content { get; set; }
+        public virtual  Option Options { get; set; }
+        public virtual Survey Survey { get; set; }
+    }
+}
